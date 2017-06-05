952206eb9b3b02f9020a61965d667a193de5a473
ankitadhar <ankitadhar12@gmail.com>
2017-06-05 19:27:44 +0530

    UI inital

--- docxmodifier/docxmodifier/urls.py
+++ docxmodifier/docxmodifier/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.http import HttpResponseRedirect
 
+from . import settings
 from views import ModifyFile
 
 urlpatterns = patterns('',
@@ -10,4 +11,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^api/modify$', ModifyFile.as_view()),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^rngen/(?P<path>.*)$', 'django.views.static.serve',
+            { 'document_root': settings.UI_ROOT, }),
+    url(r'^$', lambda r: HttpResponseRedirect('rngen/index.html')),
 )
