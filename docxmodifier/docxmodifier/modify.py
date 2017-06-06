59abd8fb671be0466757e066ec681fad2ae6bd44
Girish V <girish.v@maplelabs.com>
2017-06-06 15:36:31 +0530

    added table replace

--- docxmodifier/docxmodifier/modify.py
+++ docxmodifier/docxmodifier/modify.py
@@ -1,16 +1,22 @@
 from docx import Document
-def replace_string(filename, data):
-    replace_data = data["replace"]
-    doc = Document(filename)
+
+def modify_paragraph(doc, replace_data):
     for p in doc.paragraphs:
         for old_text, new_text in replace_data.iteritems():
             if old_text in p.text:
                 inline = p.runs
-                # Loop added to work with runs (strings with same style)
                 for i in range(len(inline)):
                     if old_text in inline[i].text:
                         text = inline[i].text.replace(old_text, new_text)
                         inline[i].text = text
                 #print p.text
+def replace_string(filename, data):
+    replace_data = data["replace"]
+    doc = Document(filename)
+    modify_paragraph(doc, replace_data)
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                modify_paragraph(cell, replace_data)
     doc.save('modify_input.docx')
     return 1
